97587d58669c0c525299510cc541f0a2ac383ca2
unknown <aureliendestailleur@outlook.fr>
2023-09-12 19:11:18 +0200

    switch projet to a other repo git

---
+++ frontend/src/components/companyData.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <b-card no-body>
+            <b-card-header header-tag="nav">
+                <b-nav card-header tabs>
+                    <b-nav-item
+                        v-for="(item, index) in companyData"
+                        :key="index"
+                        :active="index === activeTabIndex"
+                        @click="activeTabIndex = index"
+                    >
+                        {{ item.name }}
+                    </b-nav-item>
+                </b-nav>
+            </b-card-header>
+
+            <b-card-body class="text-center">
+                <b-card-title>{{ companyData[activeTabIndex].name }}</b-card-title>
+
+                <b-card-text>
+                    {{ companyData[activeTabIndex].present }}
+                </b-card-text>
+
+                <b-button variant="primary">Edit</b-button>
+            </b-card-body>
+        </b-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'companyData',
+        data() {
+            return {
+                companyData: [
+                    {
+                        name: 'Apple',
+                        present:
+                            'Apple Inc est une entreprise multinationale américaine qui crée et commercialise des produits électroniques grand public, des ordinateurs personnels et des logiciels.',
+                    },
+                    {
+                        name: 'About us',
+                        present:
+                            'Apple commercialise des produits électroniques grand public, des ordinateurs personnels et des logiciels.',
+                    },
+                    {
+                        name: 'WebSite',
+                        present:
+                            'https://www.apple.com/fr',
+                    },
+                    // Ajoutez d'autres données d'entreprise ici
+                ],
+                activeTabIndex: 0, // Indice de l'onglet actif
+    };
+        }
+    }
+</script>
---
+++ frontend/src/components/ongletData.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+      <h1>Liste des utilisateurs</h1>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nom</th>
+            <th>Identifiant</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.id">
+            <td>{{ user.id }}</td>
+            <td>{{ user.name }}</td>
+            <td>{{ user.username }}</td>
+            <td>{{ user.email }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+</template>
+  
+
+<script>
+    export default {
+        name: 'OngletData',
+        data() {
+            return {
+                users: [],
+            };
+        },
+        mounted() {
+            this.fetchData();
+        },
+        methods: {
+            fetchData() {
+            fetch("https://jsonplaceholder.typicode.com/users")
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    this.users = data;
+                })
+                .catch(error => {
+                    console.error("ERREUR :", error);
+                })
+            }
+        },
+    }
+</script>
